3acd80a6698431226da179ed3a304fc0afbaf2e5
Oleh Skoropad <notaruta@gmail.com>
2018-08-30 13:00:50 +0300

    Update apache_ssl_config.sh

--- apache_ssl_config.sh
+++ apache_ssl_config.sh
@@ -13,8 +13,8 @@ CERT_PATH=
 ### Config SSL on Apache server
 
 
+### Enable apache SSL module and restart apache
 
 
-### See the manual: 
+##### See the manual: 
 #https://github.com/borisusov/wp-host/blob/usov/README.md -> Documentation: 
-
